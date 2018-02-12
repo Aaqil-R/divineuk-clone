27ae68bbbed442f706d9bc960469900526f0c8d1
Nishan Mills <nishan@thesmallaxe.com>
2018-02-12 19:40:21 +0530

    Removing the logo; as the header image doesn't have a black background.

--- docroot/sites/all/themes/divine2013/templates/page.tpl.php
+++ docroot/sites/all/themes/divine2013/templates/page.tpl.php
@@ -73,7 +73,7 @@
 <div id="page">
 
   <header id="header" role="banner" style='background-image:url(<?php echo $header_image; ?>)'>
-   <div id="logo"><h1><a href="/uk/">Divine Chocolate</a></h1></div>
+   <!-- <div id="logo"><h1><a href="/uk/">Divine Chocolate</a></h1></div> -->
    
    <?php print render($page['header']); ?>
    
